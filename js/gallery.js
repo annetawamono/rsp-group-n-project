2ea2a75983774e3c221e7691277eef1cb10655d7
Anneta Wamono <annetawamono@yahoo.com.au>
2020-08-08 11:30:20 -0700

    gallery page done, but mobile has a few issues

--- js/gallery.js
+++ js/gallery.js
@@ -1,5 +1,12 @@
 "use strict";
-
 window.onload = function () {
-  
+  document.getElementById('menu').addEventListener("click", (e) => {
+    e.preventDefault();
+    openMobileMenu();
+  });
+
+  document.getElementById('close').addEventListener("click", (e) => {
+    e.preventDefault();
+    closeMobileMenu();
+  });
 }
