dbd986848b48018c129fae2e20201c3963760f49
Mihir Jariwala <39168691+mihirjari@users.noreply.github.com>
2020-08-10 18:16:44 +0530

    Few adjustments and Mobile Navigation updated.

---
+++ js/home.js
@@ -0,0 +1,16 @@
+"use strict";
+
+window.onload = function(){
+
+    document.getElementById("menu").onclick = function(e){
+
+        e.preventDefault();
+        openMobileMenu();
+    }
+
+    document.getElementById("close").onclick = function(e){
+
+        e.preventDefault();
+        closeMobileMenu();
+    }
+}
